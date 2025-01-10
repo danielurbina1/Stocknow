48d77bb3d8a66ba270c31c7dc4ea12030f645352
Daniel <turbinamoreno@gmail.com>
2025-01-10 13:35:46 +0100

    .

--- Stocknow/src/components/Header.jsx
+++ Stocknow/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import logo from "../imagenes/logo.png"; // Importa la imagen
 
 const Header = () => {
   const navigate = useNavigate();
@@ -73,6 +74,8 @@ const Header = () => {
     <header className="flex shadow-lg py-4 px-4 sm:px-10 bg-gradient-to-b from-gray-900 via-gray-800 to-gray-700 text-gray-200 font-[sans-serif] min-h-[70px] tracking-wide relative z-50 border-b border-gray-600">
       <div className="flex flex-wrap items-center justify-between gap-4 w-full">
         <div className="flex items-center lg:absolute lg:top-2/4 lg:left-2/4 lg:-translate-x-1/2 lg:-translate-y-1/2 max-lg:left-5">
+          <img src={logo} alt="Logo" className="w-20 h-16 mr-2" />{" "}
+          {/* Logo a la izquierda */}
           <h2 className="text-lg font-bold">StockNow</h2>
         </div>
 
